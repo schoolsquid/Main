db4d34e5c95340161274cde6e802d1bb83962d99
Pikachubolk <58044069+Pikachubolk@users.noreply.github.com>
2022-07-03 13:58:53 +0200

    Update index.js

--- newstesting/index.js
+++ newstesting/index.js
@@ -1,12 +1,1 @@
-    function news() {
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      var newsfeed = JSON.parse(this.responseText);
-      document.getElementById("news").innerHTML =
-      this.responseText;
-    }
-  };
-  xhttp.open("GET", "https://launcher.xsolla.com/projects/e6f08bc8-d32a-495b-85d9-9244fc003ff9/news?locale=en&region=us&game_id=12933&show_drafts=false&show_unpublished=false&date_from=1490895164&limit=20", false);
-  xhttp.send();
-};
+
